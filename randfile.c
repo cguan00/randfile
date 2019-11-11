f638b235b69ae0e416c083591dad29821e808412
Christy Guan <cguan00@stuy.edu>
2019-11-10 20:42:27 -0500

    set up files

---
+++ randfile.c
@@ -0,0 +1,3 @@
+#include "randfile.h"
+
+//A function that generates a random number by reading in the correct number of bytes from /dev/random.
---
+++ randfile.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//A function that generates a random number by reading in the correct number of bytes from /dev/random.

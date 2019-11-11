2dd2c150c9695993c58be87d4b8e893c4a32d2b9
Christy Guan <cguan00@stuy.edu>
2019-11-11 14:48:47 -0500

    should be complete

--- randfile.c
+++ randfile.c
@@ -1,3 +1,14 @@
 #include "randfile.h"
 
 //A function that generates a random number by reading in the correct number of bytes from /dev/random.
+
+int getRandNum(){
+  int fd = open("/dev/random", O_RDONLY);
+  int num;
+  read(fd, &num, sizeof(num));
+  if (errno){
+    printf("Errno: %s\n", strerror(errno));
+  }
+  close(fd);
+  return num;
+}
--- randfile.h
+++ randfile.h
@@ -1,4 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
 
 //A function that generates a random number by reading in the correct number of bytes from /dev/random.
+int getRandNum();
